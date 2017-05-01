4d0f53f0756f9591e8aa4ede5b3e0dafeef59989
Johnathan Hinebrook <johnisadept@live.com>
2017-05-01 16:14:37 -0400

    Update Trololo.py

--- scraper/V2/Trololo.py
+++ scraper/V2/Trololo.py
@@ -1,40 +1,26 @@
 #Johnathan Hinebrook
 #Python 3.X
-import os,sys,time,math,urllib.request,urllib.parse,re,subprocess
 
-URL = "https://en.wikipedia.org/wiki/World_War_II" #test address
+TestURL = "https://www.python.org/" #test address
 filename = 'test.html'
 browserpath = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
 
-def site(inputofplayers,filename):
-    print("Going out to the web")
-    try:
-        x = urllib.request.urlopen(inputofplayers)
-        req = urllib.request.Request(inputofplayers, headers={'User-Agent': 'Mozilla/5.0'})
-        resp = urllib.request.urlopen(req)
-        respData = resp.read()
-        print("Pulling Raw HTML")
-        file = open(filename, "w+")
-        file.write(str(respData))
-        file.close()
-        print("Exiting Site")
-    except Exception as t:
-        print("ERROR")
-        print(str(t))
    
+def site(URL,FileName,FileType):
+    import urllib.request
+    filename=FileName+FileType
+    urllib.request.urlretrieve(URL, filename)
 
+def openScrape(URL):
+    import webbrowser
+    webbrowser.open_new(URL)
 
-def main():
-    print("Site")
-    #playerinput = input()
-    playerinput = URL
-    print("Dirname")
-    site(playerinput,filename)
-    print("Done...")
-    #os.startfile(filename)
-    subprocess.Popen("%s %s" % (browserpath, filename))
-
-main()
-
-
-
+USRinput = input("Please enter address you would like to go to\n(Keep blank to test)\n\n:")
+if(USRinput == ""):
+    URL = TestURL
+try:
+    site(URL,filename,'.html')
+except:
+    print("Error")
+print("Done...")
+openScrape(filename)
